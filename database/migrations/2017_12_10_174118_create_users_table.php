4701afa6c58ff7794049576e66999bd137fe8615
Ilias Karamanis <karamanisweb+git@gmail.com>
2017-12-10 18:00:12 +0200

    Added Users Table Migration

---
+++ database/migrations/2017_12_10_174118_create_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+	/**
+	* Run the migrations.
+	*
+	* @return void
+	*/
+	public function up()
+	{
+		Schema::create('users', function (Blueprint $table) {
+			$table->increments('id');
+			$table->string('name');
+			$table->string('email');
+			$table->string('fbtoken')->unique();
+			$table->string('fbuid')->unique();
+			$table->string('picture');
+			$table->string('link');
+			$table->boolean('is_active')->default(true);
+			$table->rememberToken();
+			$table->timestamp('fbtoken_expires');
+			$table->timestamps();
+
+			$table->index('fb_uid');
+		});
+	}
+
+	/**
+	* Reverse the migrations.
+	*
+	* @return void
+	*/
+	public function down()
+	{
+		Schema::dropIfExists('users');
+	}
+}
